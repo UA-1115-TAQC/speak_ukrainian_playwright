db41c6bce1ec8acda79620466ccf67bf3ad7124f
Olena <yelenastankevych@gmail.com>
2024-04-02 23:42:17 +0300

    added comment to the news_card_component.py

--- speak_ukrainian/src/components/news_card_component.py
+++ speak_ukrainian/src/components/news_card_component.py
@@ -6,6 +6,7 @@ class NewsCardComponent(BaseComponent):
     def __init__(self, locator: Locator):
         super().__init__(locator)
 
+    #  NewsPage will be returned
     def click_news_card_link(self):
         self.locator.get_by_role("link", name="Детальніше ", exact=False).click()
 
