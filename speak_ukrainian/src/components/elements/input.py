23b61152e20a235ac99efe6545fe427aee00bbdb
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-04-02 11:22:59 +0300

    fix

--- speak_ukrainian/src/components/elements/input.py
+++ speak_ukrainian/src/components/elements/input.py
@@ -1,3 +1,6 @@
+from typing import Self
+
+
 class Input:
     def __init__(self, page):
         self.page = page
@@ -12,3 +15,13 @@ class Input:
     def get_input_value(self, selector_name) -> str:
         input_element = self.input().locator(selector_name).first
         return input_element.get_attribute("value")
+
+    def clear_input(self, selector_name) -> Self:
+        self.input().locator(selector_name).click()
+        self.page.keyboard.press("Control+A")
+        self.page.keyboard.press("Backspace")
+        self.page.keyboard.down("Meta")
+        self.page.keyboard.press("a")
+        self.page.keyboard.up("Meta")
+        self.page.keyboard.press("Backspace")
+        return self
