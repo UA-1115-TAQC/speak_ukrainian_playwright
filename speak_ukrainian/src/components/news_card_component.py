fe72980ae8dd16603ba8d8cf5334c8ae21019554
Olena <yelenastankevych@gmail.com>
2024-04-02 23:38:30 +0300

    implemented all_news_page.py and news_card_component.py

---
+++ speak_ukrainian/src/components/news_card_component.py
@@ -0,0 +1,23 @@
+from playwright._impl._locator import Locator
+from ..base import BaseComponent
+
+
+class NewsCardComponent(BaseComponent):
+    def __init__(self, locator: Locator):
+        super().__init__(locator)
+
+    def click_news_card_link(self):
+        self.locator.get_by_role("link", name="Детальніше ", exact=False).click()
+
+    @property
+    def get_news_image_locator(self) -> Locator:
+        return self.locator.locator("#newsImage")
+
+    @property
+    def get_news_date_locator(self) -> Locator:
+        return self.locator.locator("#newsDate")
+
+    @property
+    def get_news_title_locator(self) -> Locator:
+        return self.locator.locator("#newsTitle")
+
