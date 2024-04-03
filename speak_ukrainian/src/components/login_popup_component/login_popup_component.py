db020bb9cf5b022520f02b2592acb0e2e35e4972
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-04-03 11:18:37 +0300

    statics init in constr and added to method

--- speak_ukrainian/src/components/login_popup_component/login_popup_component.py
+++ speak_ukrainian/src/components/login_popup_component/login_popup_component.py
@@ -9,43 +9,71 @@ from speak_ukrainian.src.components.elements.input_with_icon_element import Inpu
 class LoginPopUpComponent(BaseComponent):
     def __init__(self, locator: Locator):
         super().__init__(locator)
+        self._login_popup_title = None
+        self._google_icon = None
+        self._facebook_icon = None
+        self._authorization_by_google = None
+        self._authorization_by_facebook = None
+        self._label_or_title = None
+        self._email_title = None
+        self._password_title = None
+        self._sing_in_button = None
+        self._restore_password_button = None
 
     @property
     def login_popup_title(self) -> Locator:
-        return self.locator.get_by_text("Вхід", exact=True)
+        if self._login_popup_title is None:
+            self._login_popup_title = self.locator.get_by_text("Вхід", exact=True)
+        return self._login_popup_title
 
     @property
     def authorization_by_google(self) -> Locator:
-        return self.locator.locator("a[href*=google]")
+        if self._authorization_by_google is None:
+            self._authorization_by_google = self.locator.locator("a[href*=google]")
+        return self._authorization_by_google
 
     @property
     def authorization_by_facebook(self) -> Locator:
-        return self.locator.locator("a[href*=facebook]")
+        if self._authorization_by_facebook is None:
+            self._authorization_by_facebook = self.locator.locator("a[href*=facebook]")
+        return self._authorization_by_facebook
 
     @property
     def google_icon(self) -> Locator:
-        return self.locator.locator("img.logo[src*='google.png']")
+        if self._google_icon is None:
+            self._google_icon = self.locator.locator("img.logo[src*='google.png']")
+        return self._google_icon
 
     @property
     def facebook_icon(self) -> Locator:
-        return self.locator.locator("img.logo[src*='facebook.png']")
+        if self._facebook_icon is None:
+            self._facebook_icon = self.locator.locator("img.logo[src*='facebook.png']")
+        return self._facebook_icon
 
     @property
     def or_tittle_text(self) -> Locator:
-        return self.locator.get_by_text("або", exact=True)
+        if self._label_or_title is None:
+            self._label_or_title = self.locator.get_by_text("або", exact=True)
+        return self._label_or_title
 
     @property
     def email_title(self) -> Locator:
-        return self.locator.get_by_text("Емейл", exact=True)
+        if self._email_title is None:
+            self._email_title = self.locator.get_by_text("Емейл", exact=True)
+        return self._email_title
 
     @property
     def password_title(self) -> Locator:
-        return self.locator.get_by_text("Пароль", exact=True)
+        if self._password_title is None:
+            self._password_title = self.locator.get_by_text("Пароль", exact=True)
+        return self._password_title
 
     @property
     def sing_in_button(self) -> Locator:
         # sign_in_button = self.locator.get_by_role("button", name="Увійти")
-        return self.locator.locator("button[class*=login-button]")
+        if self._sing_in_button is None:
+            self._sing_in_button = self.locator.locator("button[class*=login-button]")
+        return self._sing_in_button
 
     def click_sign_in_button(self) -> None:
         self.sing_in_button.click()
@@ -53,7 +81,9 @@ class LoginPopUpComponent(BaseComponent):
     @property
     def restore_password_button(self) -> Locator:
         # restore_password_button = self.locator.get_by_role("link", name="Забули пароль?")
-        return self.locator.locator("a.restore-password-button")
+        if self._restore_password_button is None:
+            self._restore_password_button = self.locator.locator("a.restore-password-button")
+        return self._restore_password_button
 
     @property
     def open_restoration_password_popup(self) -> 'RestorationPasswordPopup':
@@ -78,14 +108,21 @@ class LoginPopUpComponent(BaseComponent):
 class RestorationPasswordPopup(BaseComponent):
     def __init__(self, locator: Locator):
         super().__init__(locator)
+        self._restoration_password_title = None
+        self._close_button = None
+        self._restore_button = None
 
     @property
     def restoration_password_title(self) -> Locator:
-        return self.locator.get_by_text("Відновлення", exact=True)
+        if self._restoration_password_title is None:
+            self._restoration_password_title = self.locator.get_by_text("Відновлення", exact=True)
+        return self._restoration_password_title
 
     @property
     def close_button(self) -> Locator:
-        return self.locator.get_by_label("Close").last
+        if self._close_button is None:
+            self._close_button = self.locator.get_by_label("Close").last
+        return self._close_button
 
     @property
     def restore_email_input_element(self) -> InputWithIconElement:
@@ -100,7 +137,9 @@ class RestorationPasswordPopup(BaseComponent):
 
     @property
     def restore_button(self) -> Locator:
-        return self.locator.get_by_role("button", name="Відновити")
+        if self._restore_button is None:
+            self._restore_button = self.locator.get_by_role("button", name="Відновити")
+        return self._restore_button
 
     def click_restore_button(self) -> None:
         self.restore_button.click()
