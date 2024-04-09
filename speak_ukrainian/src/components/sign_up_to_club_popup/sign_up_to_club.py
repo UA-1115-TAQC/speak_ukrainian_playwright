b2fece08b65b8e8cd2c47c72ba4525160edd645c
Alevtyna Parysta <luxor952@gmail.com>
2024-04-09 19:14:18 +0200

    added write_to_manager_popup

--- speak_ukrainian/src/components/sign_up_to_club_popup/sign_up_to_club.py
+++ speak_ukrainian/src/components/sign_up_to_club_popup/sign_up_to_club.py
@@ -1,9 +1,9 @@
 from playwright._impl._locator import Locator
 
-from speak_ukrainian.src.base import BaseComponent
+from speak_ukrainian.src.base import BasePopUp
 
 
-class SignUpToClub(BaseComponent):
+class SignUpToClub(BasePopUp):
     def __init__(self, locator: Locator):
         super().__init__(locator)
         self.add_child_button = None
@@ -12,6 +12,6 @@ class SignUpToClub(BaseComponent):
 
     @property
     def add_child_button(self) -> Locator:
-        if not self.add_child_button:
-            self.add_child_button = (self.locator.get_by_role("button", name="plus Додати дитину"))
+        if self.add_child_button is None:
+            self.add_child_button = self.locator.get_by_role("button", name="plus Додати дитину")
             return self.add_child_button
--- speak_ukrainian/src/pages/club_page.py
+++ speak_ukrainian/src/pages/club_page.py
@@ -2,6 +2,8 @@ from playwright._impl._locator import Locator
 from playwright._impl._page import Page
 
 from speak_ukrainian.src.base import BasePage
+from speak_ukrainian.src.components.sign_up_to_club_popup.sign_up_to_club import SignUpToClub
+from speak_ukrainian.src.components.write_to_manager_popup.write_to_manager_component import WriteToManagerPopUp
 
 
 class ClubPage(BasePage):
@@ -11,16 +13,23 @@ class ClubPage(BasePage):
         self.write_to_manager = None
         self.sign_up_to_club = None
         self.leave_comment = None
-        self.show_more_comment = None
 
     @property
     def sign_up_to_club_button(self) -> Locator:
-        if not self.sign_up_to_club:
+        if self.sign_up_to_club is None:
             self.sign_up_to_club = (self.page.get_by_role("button", name="Записатись на гурток"))
             return self.sign_up_to_club
 
-
-
-    def click_sign_up_club_button(self) -> AddCenterStepTwo:
+    def click_sign_up_club_button(self) -> SignUpToClub:
         self.sign_up_to_club_button.click()
-        return AddCenterStepTwo(self.locator)
+        return SignUpToClub(self.page.locator("div.SignUpForClub_signUpForClubModal"))
+
+    @property
+    def write_to_manager_button(self) -> Locator:
+        if self.write_to_manager is None:
+            self.write_to_manager = (self.page.get_by_role("button", name="Написати менеджеру"))
+            return self.write_to_manager
+
+    def click_write_to_manager_button(self) -> WriteToManagerPopUp:
+        self.write_to_manager_button.click()
+        return WriteToManagerPopUp(self.page.locator("div.ant-modal-content"))
