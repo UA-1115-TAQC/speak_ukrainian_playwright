2243349a3610876550d33facbae8d095d5ac4e64
Anastasiia <anastasiiak55@gmail.com>
2024-04-10 23:36:35 +0300

    debugged

--- speak_ukrainian/tests/ui/components/registration_popup/test_registration_cancellation.py
+++ speak_ukrainian/tests/ui/components/registration_popup/test_registration_cancellation.py
@@ -8,12 +8,12 @@ def test_redirection_after_registration_cancelled(page):
     current_page = HomePage(page).header.click_news_button()
     start_url = current_page.page.url
 
-    register_popup = current_page.header.open_guest_menu.open_register_form()
-    register_popup.firstname_input_element.set_input_value("Qwerty")
-    register_popup.lastname_input_element.set_input_value("Qwerty")
-    register_popup.phone_input_element.set_input_value("0123456789")
-    register_popup.email_input_element.set_input_value("qwerty@email.com")
-    register_popup.click_close_button()
+    registration_popup = current_page.header.open_guest_menu.open_register_form
+    registration_popup.firstname_input_element.set_input_value("Qwerty")
+    registration_popup.lastname_input_element.set_input_value("Qwerty")
+    registration_popup.phone_input_element.set_input_value("0123456789")
+    registration_popup.email_input_element.set_input_value("qwerty@email.com")
+    registration_popup.click_close_button()
 
-    expect(page).toHaveURL(start_url)
+    expect(page).to_have_url(start_url)
 
