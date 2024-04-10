fb3e8df492de0b5b03ee6a39d3c8ed0e2849f6f7
Anastasiia <anastasiiak55@gmail.com>
2024-04-10 23:15:16 +0300

    implemented test_redirection_after_registration_cancelled

--- speak_ukrainian/tests/ui/components/registration_popup/test_registration_cancellation.py
+++ speak_ukrainian/tests/ui/components/registration_popup/test_registration_cancellation.py
@@ -1,24 +1,19 @@
-from dotenv import load_dotenv
-import pytest
+from speak_ukrainian.src.pages.home_page import HomePage
 from speak_ukrainian.tests.conftest import page
+from playwright.sync_api import expect
 
-load_dotenv()
 
-class TestRegistrationCancellation():
-    page()
+# TUA-876
+def test_redirection_after_registration_cancelled(page):
+    current_page = HomePage(page).header.click_news_button()
+    start_url = current_page.page.url
 
-    # TUA-876
-    def test_redirection_after_registration_cancelled(self):
-        header = self.homepage.header
-        current_page = header.click_news_button()
-        url = self.driver.current_url
+    register_popup = current_page.header.open_guest_menu.open_register_form()
+    register_popup.firstname_input_element.set_input_value("Qwerty")
+    register_popup.lastname_input_element.set_input_value("Qwerty")
+    register_popup.phone_input_element.set_input_value("0123456789")
+    register_popup.email_input_element.set_input_value("qwerty@email.com")
+    register_popup.click_close_button()
 
-        register_popup = header.click_profile_button().open_registration_form()
-        register_popup.firstname_input_element.set_input_value("Qwerty")
-        register_popup.lastname_input_element.set_input_value("Qwerty")
-        register_popup.phone_input_element.set_input_value("0123456789")
-        register_popup.email_input_element.set_input_value("qwerty@email.com")
-        register_popup.click_close_button()
+    expect(page).toHaveURL(start_url)
 
-        new_url = self.driver.current_url
-        self.assertEqual(url, new_url)
