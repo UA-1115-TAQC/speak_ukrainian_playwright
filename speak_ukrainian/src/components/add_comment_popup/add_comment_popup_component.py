ce00a65ff61863c480ef4fc87023178a46dec504
Alevtyna Parysta <luxor952@gmail.com>
2024-04-10 18:37:09 +0200

    added methods to components

--- speak_ukrainian/src/components/add_comment_popup/add_comment_popup_component.py
+++ speak_ukrainian/src/components/add_comment_popup/add_comment_popup_component.py
@@ -1,9 +1,55 @@
 from playwright._impl._locator import Locator
 
 from speak_ukrainian.src.base import BasePopUp
+from typing import Self
 
 
 class AddCommentPopUpComponent(BasePopUp):
     def __init__(self, locator: Locator):
         super().__init__(locator)
+        self._complain_tab = None
+        self._comment_tab = None
+        self._add_comment = None
+        self._submit_button = None
+        self._rating = None
 
+    @property
+    def complain_tab(self) -> Locator:
+        self._complain_tab = self.locator.get_by_role("tab", name="Скарга")
+        return self._complain_tab
+
+    def click_on_complain_tab(self) -> Self:
+        self.complain_tab.click()
+        return self
+
+    @property
+    def comment_tab(self) -> Locator:
+        self._add_comment = self.locator.get_by_role("tab", name="Коментар")
+        return self._comment_tab
+
+    def click_on_comment_tab(self) -> Self:
+        self.comment_tab.click()
+        return self
+
+    @property
+    def add_comment(self) -> Locator:
+        self._add_comment = self.locator.get_by_placeholder("Додайте коментар")
+        return self._add_comment
+
+    def fill_comment_input(self, comment: str) -> Self:
+        self.add_comment.fill(comment)
+        return self
+
+    @property
+    def submit_button(self) -> Locator:
+        self._submit_button = self.get_by_role("button", name="Надіслати")
+        return self._submit_button
+
+    def click_submit_button(self) -> Self:
+        self.submit_button.click()
+        return self
+
+    @property
+    def rating_star(self) -> Locator:
+        self._rating = self.locator.locator("#comment-edit_rate").get_by_role("radio", name="star star")
+        return self._rating
--- speak_ukrainian/src/components/sign_up_to_club_popup/sign_up_to_club.py
+++ speak_ukrainian/src/components/sign_up_to_club_popup/sign_up_to_club.py
@@ -2,14 +2,15 @@ from playwright._impl._locator import Locator
 
 from speak_ukrainian.src.base import BasePopUp
 from speak_ukrainian.src.components.sign_up_to_club_popup.add_child import AddChildPopUp
+from typing import Self
 
 
 class SignUpToClub(BasePopUp):
     def __init__(self, locator: Locator):
         super().__init__(locator)
         self._add_child_button = None
-        self.add_comment = None
-        self.submit_button = None
+        self._add_comment = None
+        self._submit_button = None
 
     @property
     def add_child_button(self) -> Locator:
@@ -21,4 +22,30 @@ class SignUpToClub(BasePopUp):
         self.add_child_button.click()
         return AddChildPopUp(self.locator)
 
+    @property
+    def children_list(self) -> list[Locator]:
+        return self.locator.locator(".ant-checkbox-group > div:nth-child").all()
 
+    def get_children_list_names(self) -> list[str]:
+        return [children.text_content() for children in self.children_list]
+
+    def click_child_checkbox_by_index(self, index: int) -> Self:
+        self.children_list[index].click()
+        return self
+
+    @property
+    def comment_input_element(self) -> Locator:
+        self._add_comment = self.locator.get_by_placeholder("Додати коментар")
+        return self._add_comment
+
+    def fill_comment_input(self, comment: str) -> Self:
+        return self.comment_input_element.fill(comment)
+
+    @property
+    def submit_button_element(self) -> Locator:
+        self._submit_button = self.locator.get_by_role("button", name="Записати", exact=True)
+        return self._submit_button
+
+    def click_submit_button(self) -> Self:
+        self.submit_button_element.click()
+        return self
