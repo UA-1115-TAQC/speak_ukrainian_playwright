f862dab4a54b47667503028a69a40f5712d349fa
Anastasiia <anastasiiak55@gmail.com>
2024-04-11 18:32:18 +0300

    rafactored

--- speak_ukrainian/tests/ui/components/add_club_popup/test_add_club_popup_with_admin.py
+++ speak_ukrainian/tests/ui/components/add_club_popup/test_add_club_popup_with_admin.py
@@ -5,7 +5,6 @@ import pytest
 
 # TUA-312
 def test_display_add_club_popup(page_with_admin):
-    # add_club_popup = HomePage(page).header.click_add_club_button()
     add_club_popup = page_with_admin.header.click_add_club_button()
     expect(add_club_popup.locator).to_be_visible()
 
@@ -22,7 +21,6 @@ valid_club_names = ["0123456789",
 
 @pytest.mark.parametrize("valid_name", valid_club_names)
 def test_valid_club_name(page_with_admin,valid_name):
-    # add_club_popup = HomePage(page).header.click_add_club_button()
     add_club_popup = page_with_admin.header.click_add_club_button()
     step_one = add_club_popup.step_one_container
 
