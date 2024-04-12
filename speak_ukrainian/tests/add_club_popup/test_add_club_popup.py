3f67780349cfe9be001cf10c9320016fecf843e4
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-04-12 11:27:34 +0300

    implemented TUA-121

--- speak_ukrainian/tests/add_club_popup/test_add_club_popup.py
+++ speak_ukrainian/tests/add_club_popup/test_add_club_popup.py
@@ -3,6 +3,13 @@ from playwright.sync_api import expect
 
 from speak_ukrainian.src.pages.home_page import HomePage
 
+VALID_CLUB_NAME = "Hip Hop"
+CATEGORY = "Танці, хореографія"
+TEXT_50_SYMBOLS = "Abcd " * 10
+VALID_MIN_AGE = "6"
+VALID_MAX_AGE = "30"
+VALID_TELEPHONE_NUMBER = "0987656453"
+
 
 @allure.issue("TUA-121")
 @allure.description("Verify that a club without center is created if all parameters are filled with valid values")
@@ -20,14 +27,26 @@ def test_add_club_without_center(page_with_manager: HomePage):
     assert step_one.max_age_input_element.get_input_value() == ""
     assert step_one.center_dropdown_element.placeholder.text_content() == "Назва центру"
 
-    step_one.name_input_element.set_input_value("Hip Hop")
-    step_one.click_on_category_by_name("Танці, хореографія")
-    step_one.min_age_input_element.set_input_value("6")
-    step_one.max_age_input_element.set_input_value("30")
+    step_one.name_input_element.set_input_value(VALID_CLUB_NAME)
+    step_one.click_on_category_by_name(CATEGORY)
+    step_one.min_age_input_element.set_input_value(VALID_MIN_AGE)
+    step_one.max_age_input_element.set_input_value(VALID_MAX_AGE)
 
     step_two = step_one.click_next_step_button()
     step_two.popup_title.text_content()
 
     assert step_two.is_switch_button_checked() is False
-
     assert step_two.checked_work_days_list == []
+    assert step_two.telephone_input_element.get_input_value() == ""
+    assert step_two.facebook_input_element.get_input_value() == ""
+    assert step_two.whatsapp_input_element.get_input_value() == ""
+    assert step_two.email_input_element.get_input_value() == ""
+    assert step_two.skype_input_element.get_input_value() == ""
+    assert step_two.site_input_element.get_input_value() == ""
+
+    step_two.telephone_input_element.set_input_value(VALID_TELEPHONE_NUMBER)
+    step_three = step_two.click_next_step_button()
+
+    assert step_three.get_description_textarea_value() == ""
+    step_three.set_description_textarea_value(TEXT_50_SYMBOLS)
+    step_three.click_complete_button()
