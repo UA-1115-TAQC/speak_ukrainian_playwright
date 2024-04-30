cb95464b39c594ba1f8ab9e4176fb22c9f5218af
Olena <yelenastankevych@gmail.com>
2024-04-30 23:03:11 +0300

    added teardown method for club

--- speak_ukrainian/tests/api/test_edit_club.py
+++ speak_ukrainian/tests/api/test_edit_club.py
@@ -1,9 +1,14 @@
+import copy
+
 import allure
 import pytest
+from playwright.sync_api import APIRequestContext
 
 from speak_ukrainian.src.api.club_client import ClubClient
-from speak_ukrainian.src.api.schames.club import ClubResponseSchema
+from speak_ukrainian.src.api.schames.club import ClubResponseSchema, ClubResponse
 from speak_ukrainian.src.api.schames.contacts import ContactDataResponse, ContactType
+from speak_ukrainian.src.api.schames.login import SingInResponse
+
 
 invalid_contacts_data = [(ContactDataResponse(ContactType(1, 'Телефон'),
                                               '05045636583653'), 546),
@@ -11,20 +16,37 @@ invalid_contacts_data = [(ContactDataResponse(ContactType(1, 'Телефон'),
                                               'fjhujkdj@@ha.com'), 546)]
 
 
+@pytest.fixture
+def tear_down_club(api_context_with_manager, request) -> (APIRequestContext, SingInResponse, ClubResponse):
+    context, model = api_context_with_manager
+    test_club_id = 546
+    schema = ClubResponseSchema()
+
+    club_client = ClubClient(context, token=model.access_token)
+    club_response = club_client.get_club_by_id(test_club_id)
+    test_club = schema.load(club_response.json())
+
+    # Function to clear the resources
+    def clear_resource():
+        club_client.update_club(test_club_id, test_club)
+
+    request.addfinalizer(clear_resource)
+
+    return context, model, test_club
+
+
 @allure.issue("TUA-977")
 @allure.description('[API] Vefiry that user cannot save \'Контакти\' fields'
                     ' with invalid data (for club that is not in the center)')
 @allure.label("owner", "Olena Stankevych")
 @pytest.mark.parametrize('invalid_contact, club_id', invalid_contacts_data)
-def test_edit_club_contacts_with_incorrect_data(api_context_with_manager,
+def test_edit_club_contacts_with_incorrect_data(tear_down_club,
                                                 invalid_contact,
                                                 club_id):
-    context, model = api_context_with_manager
+    context, model, club = tear_down_club
     schema = ClubResponseSchema()
-
+    test_club = copy.deepcopy(club)
     club_client = ClubClient(context, token=model.access_token)
-    club_response = club_client.get_club_by_id(club_id)
-    test_club = schema.load(club_response.json())
 
     test_club.contacts = [invalid_contact]
     club_client.update_club(club_id, test_club)
